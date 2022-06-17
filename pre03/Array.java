43e18c02ad6cedfd1ed5ea675fbb4af38bf3fc55
ssecc001 <46972076+ssecc001@users.noreply.github.com>
2022-06-17 15:33:26 +0000

    Array

--- pre03/Array.java
+++ pre03/Array.java
@@ -1,18 +1,11 @@
 //Starting with the code in Section 8.6, write a method called powArray that takes a double array, a, and returns a new array that contains the elements of a squared. Generalize it to take a second argument and raise the elements of a to the given power.
 
 
-public static double[] powArray (double a[]){
+public static powArray(double a[]){
     for (int i = 0; i < a.length; i++) {
          a[i] = Math.pow(a[i], 2.0);
     }
     return a;
-}
-
-public class Array{
-public static void main(String[] args) {
-    double[] a = new double[]{5, 6, 8, 7};
-    System.out.println(Arrays.toString(powArray(a)));
-}
 }
 
 
